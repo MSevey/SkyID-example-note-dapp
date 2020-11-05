6c5d553358da66da7a1535faa97c0107b32a453d
Matthew Sevey <mjsevey@gmail.com>
2020-11-04 19:59:14 -0600

    WORKSHOP: SkyBD

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
-import { SkynetClient } from "skynet-js";
+import { SkynetClient, keyPairFromSeed } from "skynet-js";
 
 // Create the Skynet client
 const client = new SkynetClient();
 
-// uploadNote uploads the note to skylink
-window.uploadNote = function (note) {
-  // Create the file object
-  var file = new File([note], "note", { type: "text/plain" });
+// filename will be the data key for the SkyDB entry
+const filename = "editnote";
 
-  // Upload to Skynet
+// setNote uploads the note to SkyDB
+window.setNote = function (seed) {
+  const { privateKey } = keyPairFromSeed(seed);
+  var note = document.getElementById("note").value;
+
   try {
     (async () => {
-      // Upload the file
-      const skylink = await client.uploadFile(file);
+      // Upload to SkyDB
+      await client.db.setJSON(privateKey, filename, { note });
+      alert("Successfully Uploaded!");
+    })();
+  } catch (error) {
+    alert(error);
+  }
+};
 
-      // Display the Skylink to the User
-      const directLink = "/" + skylink.replace("sia:", "");
-      document.getElementById("mediaLink").href = directLink;
-      document.getElementById("mediaLink").text = skylink;
+window.getNote = function (seed) {
+  try {
+    (async () => {
+      const { publicKey } = keyPairFromSeed(seed);
+      const entry = await client.db.getJSON(publicKey, filename);
+
+      if (entry) {
+        var note = document.getElementById("note");
+        note.value = entry.data.note;
+      }
     })();
   } catch (error) {
     alert(error);
